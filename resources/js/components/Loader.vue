f6bbc52cca83163951d026626c30f5e19e364ec9
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-04-05 01:33:18 +0200

    Adds Loader

---
+++ resources/js/components/Loader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="loader" class="d-flex flex-column">
+      <div>
+        <div class="spinner-grow text-primary" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-warning mx-3" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-danger" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+      </div>
+<h3 class="text-white mt-3">Loading...</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Loader',
+}
+</script>
+
+<style>
+#loader{
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+}
+</style>
--- resources/js/components/posts/PostsList.vue
+++ resources/js/components/posts/PostsList.vue
@@ -1,29 +1,39 @@
 <template>
   <section class="m-4">
       <h2>Posts</h2>
-      <ul v-if="posts.length">
-          <li v-for="post in posts" :key="post.id">{{post.title}}</li>
-      </ul>
-      <p v-else>Non ci sono post.</p>
+      <Loader v-if="isLoading"/>
+      <div v-else>
+        <ul v-if="posts.length">
+            <li v-for="post in posts" :key="post.id">{{post.title}}</li>
+        </ul>
+        <p v-else>Non ci sono post.</p>
+      </div>
   </section>
 </template>
 
 <script>
+import Loader from '../Loader.vue';
 export default {
     name: "PostsList",
+    components: {
+        Loader,
+    },
     data(){
         return{
             posts: [],
-        }
+            isLoading: false,
+        };
     },
     methods: {
       getPosts(){
+          this.isLoading = true;
         axios.get('http://127.0.0.1:8000/api/posts').then((res) => {
           this.posts = res.data;
         }).catch((err) => {
           console.error(err);
         }).then(() => {
           console.log('Chiamata terminata');
+          this.isLoading = false;
         })
       }
     },
